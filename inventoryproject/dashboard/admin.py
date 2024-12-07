2f6decd2e65a5af9d3e0242226230d8d5ef027c4
city_hunter <mustafamallick96@gmail.com>
2024-12-07 16:36:14 -0500

    added adding more users section and adding product

--- inventoryproject/dashboard/admin.py
+++ inventoryproject/dashboard/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from  . models import Product
+from django.contrib.auth.models import Group
 
+
+admin.site.site_header = 'StrawHats Inventory Dashboard'
+
+
+
+class ProdcutAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'quantity',)
+    #list_filter = ('category',)
+    list_filter = ['category']
 
 # Register your models here.
 
-admin.site.register(Product)
+admin.site.register(Product, ProdcutAdmin)
+#admin.site.unregister(Group)
