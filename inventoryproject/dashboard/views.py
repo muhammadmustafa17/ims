a4e3e58fe8240aa4eb9d5e054dfbcfc600820414
city_hunter <mustafamallick96@gmail.com>
2024-12-13 04:17:05 -0500

    fixed the backend login of staff page, product page and order page

--- inventoryproject/dashboard/views.py
+++ inventoryproject/dashboard/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Product
+from .models import Product, Order
+from .forms import ProductForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 #@login_required(login_url='user-login')
@@ -11,10 +13,66 @@ def index(request):
 
 @login_required
 def staff(request):
-    return render(request, 'dashboard/staff.html')
+    workers = User.objects.all()
+    context={
+        'workers' : workers
+
+    }
+    return render(request, 'dashboard/staff.html', context)
+@login_required
+def staff_detail(request, pk):
+    workers = User.objects.get(id=pk)
+    context={
+        'workers': workers,
+    }
+    return render(request, 'dashboard/staff_detail.html', context)
+
 @login_required
 def product(request):
-    return render(request, 'dashboard/product.html')
+    items = Product.objects.all() #using ORM
+    #items = Product.objects.raw('SELECT * FROM dashboard_product')
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid(): 
+            form.save()
+            return redirect('dashboard-product')
+    else :
+        form = ProductForm()
+    context = {
+        'items' : items,
+        'form' : form,
+    }
+    return render(request, 'dashboard/product.html', context)
+
+@login_required
+def product_delete(request, pk):
+    item = Product.objects.get(id=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('dashboard-product')
+    return render(request, 'dashboard/product_delete.html')
+
+@login_required
+def product_update(request, pk):
+    item = Product.objects.get(id=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard-product')
+    else:
+        form = ProductForm(instance=item)
+    context={
+        'form': form,
+    }
+    return render(request, 'dashboard/product_update.html', context)
+
 @login_required
 def order(request):
-    return render(request, 'dashboard/order.html')
+    orders = Order.objects.all()
+
+    context ={
+        'orders': orders,
+    }
+    return render(request, 'dashboard/order.html', context)
+
