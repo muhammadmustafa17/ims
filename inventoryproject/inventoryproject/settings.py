d0808882c24d543afc90e1c04bc5989cc404fa96
city_hunter <mustafamallick96@gmail.com>
2024-12-13 23:50:44 -0500

    forgot password doesnt work

--- inventoryproject/inventoryproject/settings.py
+++ inventoryproject/inventoryproject/settings.py
@@ -142,4 +142,11 @@ MEDIA_URL = '/meida/'
 LOGIN_REDIRECT_URL = 'dashboard-index'
 LOGIN_URL = 'user-login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mustafamubrose@gmail.com'
+EMAIL_HOST_PASSWORD = 'Abdullah288!'
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
