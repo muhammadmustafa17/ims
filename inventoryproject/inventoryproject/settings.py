fcaa68d78c08033db3ebc214d508635bd48e8ace
city_hunter <mustafamallick96@gmail.com>
2024-12-14 21:59:10 -0500

    small updateds on forget password

--- inventoryproject/inventoryproject/settings.py
+++ inventoryproject/inventoryproject/settings.py
@@ -146,7 +146,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mustafamubrose@gmail.com'
+EMAIL_HOST_USER = '' #type your email in there
 EMAIL_HOST_PASSWORD = ''
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
